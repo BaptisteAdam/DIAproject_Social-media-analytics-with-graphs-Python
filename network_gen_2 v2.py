f43f54fc7351e3a4627b3a1ba59d1f67bb171c4a
BaptisteAdam <36033177+BaptisteAdam@users.noreply.github.com>
2020-06-18 18:07:54 +0200

    Add files via upload

---
+++ network_gen_2 v2.py
@@ -0,0 +1,184 @@
+import random
+from random import randint
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import networkx as nx
+
+# --------------------------------- #
+#      Generation of the graph      #
+# --------------------------------- #
+
+# Function that generate User's info in dict
+def generate_users(nb_of_users):
+    data = open("list_of_names.txt")
+    data2 = [x.strip() for x in data]
+    sex = ["Male", "Female", "Genderqueer/Non-Binary"]
+    color_sex = {"Male":"blue", "Female":"red", "Genderqueer/Non-Binary":"green"}
+
+    age_dict = {}
+    sex_dict = {}
+    daily_use_dict = {}
+    color_map = []
+
+    name_list = [random.choice(data2) for i in range(nb_of_users)]
+    for name in name_list:
+        age_dict[name] = randint(10, 100)
+        sex_dict[name] = random.choice(sex)
+        color_map.append(color_sex[sex_dict[name]])
+        daily_use_dict[name] = randint(0, 10)
+    return name_list, age_dict, sex_dict, daily_use_dict, color_map
+
+# Function that generates the nodes
+def node_generation(nb_of_nodes):
+    name_list, age_dict, sex_dict, daily_use_dict, color_map = generate_users(nb_of_nodes)
+    my_network = nx.MultiGraph()            ## Multigraph = Graph with multiple features per edges
+    my_network.add_nodes_from(name_list)
+    nx.set_node_attributes(my_network, age_dict, 'age')
+    nx.set_node_attributes(my_network, sex_dict, 'sex')
+    nx.set_node_attributes(my_network, daily_use_dict, 'daily_use')
+
+    return my_network, color_map
+
+# Function that addd the nodes corresponding to the special feature D (here, they are "NaN" users)
+def add_feature_D(my_network, color_map, nb):
+    #generate NaN users
+    data = open("list_of_names.txt")
+    data2 = [x.strip() for x in data]
+
+    age_dict = {}
+    sex_dict = {}
+    daily_use_dict = {}
+
+    name_list = [random.choice(data2) for i in range(nb)]
+    for name in name_list:
+        age_dict[name] = randint(10, 100)
+        sex_dict[name] = "NaN"
+        color_map.append("Grey")
+        daily_use_dict[name] = randint(0, 10)
+
+    #add NaN users to the graph
+    my_network.add_nodes_from(name_list)
+    nx.set_node_attributes(my_network, age_dict, 'age')
+    nx.set_node_attributes(my_network, sex_dict, 'sex')
+    nx.set_node_attributes(my_network, daily_use_dict, 'daily_use')
+
+    return my_network, color_map
+
+# Function that generates edges between the existing nodes
+def edge_generation(my_network, nb_of_edges):
+    while my_network.number_of_edges() < nb_of_edges:
+        e1 = random.choice(list(my_network.nodes))
+        e2 = random.choice(list(my_network.nodes))
+
+        if my_network.has_edge(e1, e2) != True and e1 != e2:
+            n1 = my_network.nodes[e1]
+            n2 = my_network.nodes[e2]
+
+            delta_age = abs(n1["age"] - n2["age"])
+            delta_daily_use = abs(n1["daily_use"] - n2["daily_use"])
+
+            my_network.add_edge(e1, e2, delta_age=delta_age, delta_daily_use=delta_daily_use)
+    return my_network
+
+# Function that position A,B on the left and the others on the right
+def position_nodes(my_network):
+    pos = {}
+    for index, node in enumerate(my_network.nodes):
+        if my_network.nodes[node]['sex'] == "Male" or my_network.nodes[node]['sex'] == "Female":
+            pos.update({node :(1, index)})
+        else:
+            pos.update({node : (2, index)})
+    return pos
+
+# Function that generates the nodes than the edges to make the whole graph
+def graph_generation(nb_nodes, nb_of_edges):
+    my_network, color_map = node_generation(nb_nodes)
+    my_network = edge_generation(my_network, nb_of_edges)
+    my_network, color_map = add_feature_D(my_network, color_map, nb_nodes//4)
+
+    return my_network, color_map
+
+
+# --------------------------------- #
+#        Tools for the graph        #
+# --------------------------------- #
+
+# Function that return the probability of an edge activation
+def prob_edge_activation(delta_age, delta_daily_use):
+    age = 1 - delta_age / 100
+    daily_use = 1 - delta_daily_use/10
+    return age * 0.5 + daily_use * 0.5
+
+# Function that draws the graph, can choose if bipartite or not
+def draw_graph(my_network, color_map, bipartite=False):
+    if bipartite:
+        pos = position_nodes(my_network)
+        nx.draw(my_network, with_labels=True, node_color=color_map, pos=pos)
+    else:
+        nx.draw(my_network, with_labels=True, node_color=color_map)
+    plt.show()
+
+def bernoulli(prob):
+    nb = randint(1, 100)
+    if nb <= prob*100:
+        return 1
+    return 0
+
+
+def SocInfl_Maximization(my_network, budgetA) :
+    seeds = []
+    node_list = list(my_network.nodes)
+    while (len(seeds) < budgetA):
+        marginal_increase_vector = []
+        node_list_aux = []
+        for node in enumerate(node_list):
+            
+            margIncNode = 4 # calculer l'esperance (en gros, le nombre moyen de node que ca va activer)
+
+            marginal_increase_vector.append(margIncNode)
+            node_list_aux.append(node[1])
+            #ajouter un if() lié à l'ajout au matching pb
+        best_node = node_list_aux[marginal_increase_vector.index(np.max(marginal_increase_vector))]
+        seeds.append(best_node)
+        node_list.remove(best_node)
+    return seeds
+
+
+# --------------------------------- #
+#         Debug (temporary)         #
+# --------------------------------- #
+def longest_edge_name(my_network):
+    network = list(my_network.nodes)
+    maxi = len(network[0])
+    max2 = len(network[1])
+
+    for name in network[2:]:
+        if len(name) > maxi:
+            maxi = len(name)
+        elif len(name) > max2:
+            max2 = len(name)
+
+    return maxi+max2+1
+
+def print_info(my_network):
+    print("\nnodes :")
+    for node in my_network.nodes.data():
+        print(node)
+
+    print("\nedges :")
+    l_e_n = longest_edge_name(my_network)
+    for edge in my_network.edges:
+        egde_name = str(edge[0])+"-"+str(edge[1])
+        while len(egde_name) < l_e_n:
+            egde_name += ' '
+        print(egde_name+" :", my_network.get_edge_data(edge[0], edge[1])[0])
+
+# --------------------------------- #
+#               MAIN                #
+# --------------------------------- #
+if __name__ == "__main__":
+    my_network, color_map = graph_generation(10, 30)
+
+    print_info(my_network)
+    draw_graph(my_network, color_map, bipartite=False)
